65cf6870a3ee20afc72594088df263920203fec8
Michał Kazimierczak <michal@flarestudio.pl>
2018-01-27 11:26:34 +0100

    I2cTests

---
+++ Commons/Exceptions/ParseException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Commons.Exceptions
+{
+    public class ParseException : Exception
+    {
+        public string StringToParse { get; }
+
+        public ParseException(Exception exception, string stringToParse)
+            : base("Unable to parse string", exception)
+        {
+            StringToParse = stringToParse;
+        }
+
+        public override string Message => $"{base.Message}: {StringToParse}";
+    }
+}
---
+++ HardwareAccess.Tests/Buses/I2cTests.cs
@@ -0,0 +1,36 @@
+﻿using HardwareAccess.Buses;
+using HardwareAccess.Buses.PlatformIntegration;
+using NSubstitute;
+using Xunit;
+
+namespace HardwareAccess.Tests.Buses
+{
+    public class I2cTests
+    {
+        [Fact]
+        public void devices_list_parsed()
+        {
+            // Arrange
+            var i2cdetectResult = "     0  1  2  3  4  5  6  7  8  9  a  b  c  d  e  f\n" +
+                                  "00:          -- -- -- -- -- -- -- -- -- -- -- -- --\n" +
+                                  "10: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --\n" +
+                                  "20: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --\n" +
+                                  "30: -- -- -- -- -- -- -- -- -- -- -- -- 3c -- -- --\n" +
+                                  "40: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --\n" +
+                                  "50: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --\n" +
+                                  "60: -- -- -- -- -- -- -- -- UU -- -- -- -- -- -- --\n" +
+                                  "70: -- -- -- -- -- -- -- --\n";
+
+            var processRunner = Substitute.For<IProcessRunner>();
+            processRunner.Run("i2cdetect", "-y 1").Returns(i2cdetectResult);
+
+            // Act
+            var i2c = new I2c(processRunner, null);
+            var i2cDevices = i2c.GetI2cDevices();
+            i2cDevices.Wait();
+
+            // Assert
+            Assert.Collection(i2cDevices.Result, id => Assert.Equal(0x3c, id));
+        }
+    }
+}
