304a46b47d5d09d8a31097ba0728bcdc80ab9a28
Michał Kazimierczak <michal@flarestudio.pl>
2018-02-18 17:13:24 +0100

    beginning of api

---
+++ HeatingApi/Controllers/SetupController.cs
@@ -0,0 +1,34 @@
+﻿using HeatingControl.Domain;
+using Microsoft.AspNetCore.Mvc;
+using Storage.BuildingModel;
+
+namespace HeatingApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("/api/setup")]
+    public class SetupController : Controller
+    {
+        private readonly IBuildingModelProvider _buildingModelProvider;
+        private readonly IBuildingModelSaver _buildingModelSaver;
+
+        public SetupController(IBuildingModelProvider buildingModelProvider,
+                               IBuildingModelSaver buildingModelSaver)
+        {
+            _buildingModelProvider = buildingModelProvider;
+            _buildingModelSaver = buildingModelSaver;
+        }
+
+        [HttpGet]
+        public Building GetBuildingModel()
+        {
+            return _buildingModelProvider.Provide();
+        }
+
+        [HttpPut]
+        public void SaveBuildingModel([FromBody] Building building)
+        {
+            // TODO validation
+            _buildingModelSaver.Save(building);
+        }
+    }
+}
---
+++ HeatingApi/Controllers/ZoneController.cs
@@ -0,0 +1,25 @@
+﻿using HeatingControl;
+using HeatingControl.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HeatingApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("/api/zone")]
+    public class ZoneController : Controller
+    {
+        private readonly IHeatingControl _heatingControl;
+
+        public ZoneController(IHeatingControl heatingControl)
+        {
+            _heatingControl = heatingControl;
+        }
+
+        [HttpGet("{name}")]
+        public TemperatureZoneState GetState(string name)
+        {
+            // TODO wrap state, don't let HC.Models to show up here
+            return _heatingControl.State.TemperatureZoneNameToState[name];
+        }
+    }
+}
