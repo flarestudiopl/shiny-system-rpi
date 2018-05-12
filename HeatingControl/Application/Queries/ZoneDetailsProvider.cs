f703dd18ec72df66c7b0cb9a1bde6f79ac34c37a
Michal Kazimierczak <michal@flarestudio.pl>
2018-05-12 16:10:08 +0200

    zone details counters

--- HeatingControl/Application/Queries/ZoneDetailsProvider.cs
+++ HeatingControl/Application/Queries/ZoneDetailsProvider.cs
@@ -4,12 +4,13 @@ using System.Linq;
 using Domain.BuildingModel;
 using HeatingControl.Extensions;
 using HeatingControl.Models;
+using Storage.StorageDatabase.Counter;
 
 namespace HeatingControl.Application.Queries
 {
     public interface IZoneDetailsProvider
     {
-        ZoneDetailsProviderResult Provide(int zoneId, ControllerState controllerState);
+        ZoneDetailsProviderResult Provide(int zoneId, ControllerState controllerState, Building building);
     }
 
     public class ZoneDetailsProviderResult
@@ -20,9 +21,8 @@ namespace HeatingControl.Application.Queries
 
         public class CountersData
         {
-            public float UsageValue { get; set; }
-            public UsageUnit UsageUnit { get; set; }
-            public DateTime LastResetDate { get; set; }
+            public IDictionary<UsageUnit, float> UsageUnitToValue { get; set; }
+            public DateTime? LastResetDate { get; set; }
             public IDictionary<DateTime, float> PlotPoints { get; set; }
         }
 
@@ -37,7 +37,14 @@ namespace HeatingControl.Application.Queries
 
     public class ZoneDetailsProvider : IZoneDetailsProvider
     {
-        public ZoneDetailsProviderResult Provide(int zoneId, ControllerState controllerState)
+        private readonly ICurrentCountersByHeaterIdsProvider _currentCountersByHeaterIdsProvider;
+
+        public ZoneDetailsProvider(ICurrentCountersByHeaterIdsProvider currentCountersByHeaterIdsProvider)
+        {
+            _currentCountersByHeaterIdsProvider = currentCountersByHeaterIdsProvider;
+        }
+
+        public ZoneDetailsProviderResult Provide(int zoneId, ControllerState controllerState, Building building)
         {
             var zone = controllerState.ZoneIdToState.GetValueOrDefault(zoneId);
 
@@ -48,7 +55,7 @@ namespace HeatingControl.Application.Queries
 
             return new ZoneDetailsProviderResult
                    {
-                       Counters = GetCountersData(zone),
+                       Counters = GetCountersData(zone, building),
                        Temperatures = GetTemperatureSettings(zone),
                        Schedule = zone.Zone
                                       .Schedule
@@ -58,9 +65,31 @@ namespace HeatingControl.Application.Queries
                    };
         }
 
-        private static ZoneDetailsProviderResult.CountersData GetCountersData(ZoneState zone)
+        private ZoneDetailsProviderResult.CountersData GetCountersData(ZoneState zone, Building building)
         {
-            return new ZoneDetailsProviderResult.CountersData(); // TODO
+            var heaterIds = zone.Zone.HeaterIds;
+            var heatersCounters = _currentCountersByHeaterIdsProvider.Provide(heaterIds)
+                                                                     .ToDictionary(x => x.HeaterId,
+                                                                                   x => x);
+
+            var heaterIdToCountedSeconds = heaterIds.ToDictionary(x => x,
+                 x =>
+                 {
+                     var savedCounterValue = heatersCounters.GetValueOrDefault(x)?.CountedSeconds ?? 0;
+                     // TODO - add current, not saved yet value
+                     return savedCounterValue;
+                 });
+
+            var usageUnitToHeaters = building.Heaters
+                                             .Where(x => heaterIds.Contains(x.HeaterId))
+                                             .GroupBy(x => x.UsageUnit);
+
+            return new ZoneDetailsProviderResult.CountersData
+                    {
+                        LastResetDate = heatersCounters.Any() ? heatersCounters.Values.Min(x => x.Start) : (DateTime?)null,
+                        UsageUnitToValue = usageUnitToHeaters.ToDictionary(x => x.Key,
+                                                                           x => x.Sum(h => h.UsagePerHour * (float)heaterIdToCountedSeconds[h.HeaterId] / 3600f))
+                    };
         }
 
         private static ZoneDetailsProviderResult.TemperatureSettings GetTemperatureSettings(ZoneState zone)
