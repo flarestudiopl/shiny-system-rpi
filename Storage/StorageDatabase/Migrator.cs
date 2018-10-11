c7478cb34400604186e79cbda6e8cacee58a0dae
Michał Kazimierczak <michal@flarestudio.pl>
2018-10-11 21:48:30 +0200

    db migrator; user by pin provider stub

---
+++ Storage/StorageDatabase/Migrator.cs
@@ -0,0 +1,40 @@
+﻿using DbUp;
+using DbUp.SQLite.Helpers;
+using System.Reflection;
+
+namespace Storage.StorageDatabase
+{
+    public interface IMigrator
+    {
+        void Run();
+    }
+
+    public class Migrator : IMigrator
+    {
+        private readonly ISqlConnectionResolver _sqlConnectionResolver;
+
+        public Migrator(ISqlConnectionResolver sqlConnectionResolver)
+        {
+            _sqlConnectionResolver = sqlConnectionResolver;
+        }
+
+        public void Run()
+        {
+            using (var connection = _sqlConnectionResolver.Resolve())
+            {
+                var upgrader = DeployChanges.To
+                                            .SQLiteDatabase(new SharedConnection(connection))
+                                            .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
+                                            .LogToConsole()
+                                            .Build();
+
+                var result = upgrader.PerformUpgrade();
+
+                if (result.Error != null)
+                {
+                    throw result.Error;
+                }
+            }
+        }
+    }
+}
