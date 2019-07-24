693cf99621cac42dcc0008a153434d9001f33121
Michał Kazimierczak <michal@flarestudio.pl>
2019-07-24 20:50:23 +0200

    mcp expander integration

---
+++ HardwareAccess.Tests/Devices/PowerOutputs/ShinyMcpExpanderTests.cs
@@ -0,0 +1,82 @@
+﻿using HardwareAccess.Buses;
+using HardwareAccess.Devices.PowerOutputs;
+using NSubstitute;
+using Xunit;
+
+namespace HardwareAccess.Tests.Devices.PowerOutputs
+{
+    public class ShinyMcpExpanderTests
+    {
+        [Fact]
+        public void can_enable_disable_specific_bit()
+        {
+            // Arrange
+            var deviceId = 32;
+            var deviceState = 0;
+            var i2c = Substitute.For<II2c>();
+
+            i2c.When(x => x.WriteToDevice(Arg.Any<int>(), Arg.Any<byte>(), Arg.Any<byte>()))
+               .Do(x => deviceState = x.ArgAt<byte>(2));
+
+            var powerOutput = new ShinyMcpExpander(i2c);
+
+            // Act
+            powerOutput.SetState(deviceId, "O1", true);  // 1000 0000
+            powerOutput.SetState(deviceId, "O3", true);  // 1010 0000
+            powerOutput.SetState(deviceId, "O6", true);  // 1010 0100
+            powerOutput.SetState(deviceId, "O3", false); // 1000 0100 => 132
+
+            // Assert
+            Assert.Equal(132, deviceState);
+            i2c.Received().WriteToDevice(deviceId, 0x15, 132);
+        }
+
+        [Fact]
+        public void can_get_state()
+        {
+            // Arrange
+            var powerOutput = new ShinyMcpExpander(null);
+
+            // Act
+            var state = powerOutput.GetState(32, "O1");
+
+            // Assert
+            Assert.Equal(false, state);
+        }
+
+        [Fact]
+        public void can_set_and_read_state()
+        {
+            // Arrange
+            var deviceId = 32;
+            var i2c = Substitute.For<II2c>();
+            var powerOutput = new ShinyMcpExpander(i2c);
+
+            // Act
+            powerOutput.SetState(deviceId, "O3", true);  // 0000 0100
+
+            // Assert
+            Assert.Equal(false, powerOutput.GetState(deviceId, "O1"));
+            Assert.Equal(false, powerOutput.GetState(deviceId, "O2"));
+            Assert.Equal(true, powerOutput.GetState(deviceId, "O3"));
+            Assert.Equal(false, powerOutput.GetState(deviceId, "O4"));
+        }
+
+        [Fact]
+        public void initializes_output_before_setting_state()
+        {
+            // Arrange
+            var deviceId = 32;
+            var i2c = Substitute.For<II2c>();
+            var powerOutput = new ShinyMcpExpander(i2c);
+
+            // Act
+            powerOutput.SetState(deviceId, "N1", true);
+
+            // Assert
+            i2c.Received().WriteToDevice(deviceId, 0x00, 0x00);
+            i2c.Received().WriteToDevice(deviceId, 0x01, 0x00);
+            i2c.Received().WriteToDevice(deviceId, 0x14, 0x02);
+        }
+    }
+}
--- HardwareAccess/Devices/PowerOutputs/ShinyMcpExpander.cs
+++ HardwareAccess/Devices/PowerOutputs/ShinyMcpExpander.cs
@@ -13,25 +13,28 @@ namespace HardwareAccess.Devices.PowerOutputs
 
     public class ShinyMcpExpander : IShinyMcpExpander
     {
-        private readonly static int[] I2C_ADDRESSES = new int[] { 20, 21, 22, 23 };
+        private readonly static HashSet<int> I2C_ADDRESSES = new HashSet<int>(new int[] { 32, 33, 34, 35 });
 
-        private readonly static IDictionary<string, int> OUTPUT_NAME_TO_CHANNEL = new Dictionary<string, int>
+        private readonly static IDictionary<string, Channel> OUTPUT_NAME_TO_CHANNEL = new Dictionary<string, Channel>
         {
-            ["O1"] = 16,
-            ["O2"] = 15,
-            ["O3"] = 14,
-            ["O4"] = 13,
-            ["O5"] = 12,
-            ["O6"] = 11,
-            ["O7"] = 10,
-            ["O8"] = 9,
-            ["O9"] = 4,
-            ["O10"] = 3,
-            ["N1"] = 2,
-            ["N2"] = 1,
+            ["O1"] = Channel.Create(Bank.B, 7),
+            ["O2"] = Channel.Create(Bank.B, 6),
+            ["O3"] = Channel.Create(Bank.B, 5),
+            ["O4"] = Channel.Create(Bank.B, 4),
+            ["O5"] = Channel.Create(Bank.B, 3),
+            ["O6"] = Channel.Create(Bank.B, 2),
+            ["O7"] = Channel.Create(Bank.B, 1),
+            ["O8"] = Channel.Create(Bank.B, 0),
+            ["O9"] = Channel.Create(Bank.A, 3),
+            ["O10"] = Channel.Create(Bank.A, 2),
+            ["N1"] = Channel.Create(Bank.A, 1),
+            ["N2"] = Channel.Create(Bank.A, 0),
         };
 
         private readonly II2c _i2c;
+
+        private readonly object _lock = new object();
+        private readonly IDictionary<int, OutputState> _deviceIdToChipState = new Dictionary<int, OutputState>();
 
         public string ProtocolName => ProtocolNames.ShinyBoard;
 
@@ -50,14 +53,93 @@ namespace HardwareAccess.Devices.PowerOutputs
                              .ToArray();
         }
 
-        public bool GetState(int deviceId, string outputName)
+        public void SetState(int deviceId, string outputName, bool newState)
         {
-            throw new NotImplementedException();
+            var channel = GetChannel(outputName);
+
+            lock (_lock)
+            {
+                var chipState = GetChipState(deviceId);
+
+                if (!chipState.InitializedAsOutput)
+                {
+                    _i2c.WriteToDevice(deviceId, 0x00, 0x00);
+                    _i2c.WriteToDevice(deviceId, 0x01, 0x00);
+                }
+
+                var bitToFlip = 1 << channel.Pin;
+
+                var currentState = chipState.BankToValue[channel.Bank];
+
+                if (newState)
+                {
+                    currentState |= bitToFlip;
+                }
+                else
+                {
+                    currentState &= ~bitToFlip;
+                }
+
+                chipState.BankToValue[channel.Bank] = currentState;
+
+                _i2c.WriteToDevice(deviceId, (byte)channel.Bank, (byte)currentState);
+            }
         }
 
-        public void SetState(int deviceId, string outputName, bool state)
+        public bool GetState(int deviceId, string outputName)
         {
-            throw new NotImplementedException();
+            var channel = GetChannel(outputName);
+            var chipState = GetChipState(deviceId);
+
+            var bitToCheck = 1 << channel.Pin;
+
+            return (chipState.BankToValue[channel.Bank] & bitToCheck) == bitToCheck;
+        }
+
+        private Channel GetChannel(string outputName)
+        {
+            if (!OUTPUT_NAME_TO_CHANNEL.TryGetValue(outputName, out var channel))
+            {
+                throw new ArgumentException(nameof(outputName));
+            }
+
+            return channel;
+        }
+
+        private OutputState GetChipState(int deviceId)
+        {
+            if (!I2C_ADDRESSES.Contains(deviceId))
+            {
+                throw new ArgumentException(nameof(deviceId));
+            }
+
+            if (!_deviceIdToChipState.TryGetValue(deviceId, out var outputState))
+            {
+                outputState = new OutputState();
+                _deviceIdToChipState[deviceId] = outputState;
+            }
+
+            return outputState;
+        }
+
+        private struct Channel
+        {
+            public Bank Bank { get; set; }
+            public int Pin { get; set; }
+
+            public static Channel Create(Bank bank, int pin) => new Channel { Bank = bank, Pin = pin };
+        }
+
+        private class OutputState
+        {
+            public bool InitializedAsOutput { get; set; }
+            public IDictionary<Bank, int> BankToValue { get; } = new Dictionary<Bank, int> { [Bank.A] = 0, [Bank.B] = 0 };
+        }
+
+        private enum Bank : byte
+        {
+            A = 0x14,
+            B = 0x15
         }
     }
 }
